a5bc95e9770119111fd926683fda292a094d155a
SatyajeetChavan30 <satyajeetchavan30@gmail.com>
2026-01-19 19:36:45 +0530

    basic express and dotenv

---
+++ Deploy/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require(`express`) //equivalent to  import experss from "express" <= can be used instead
+const app = express()             //app can be used to call express functionality
+const port = 3000
+
+app.get("/", (req,res) => {
+    res.send('hello world')
+})
+
+app.get('/tweeter', (req, res) => {
+    res.send('mytweet')
+})
+
+app.listen(process.env.port, (req, res) => {
+    console.log(`program listning on port ${port}`)
+})
