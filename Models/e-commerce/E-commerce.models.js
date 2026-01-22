0ec6dcff7ce8e04db8a37fb052ad4b2b8dd2add8
SatyajeetChavan30 <satyajeetchavan30@gmail.com>
2026-01-22 21:45:24 +0530

    ecommerce  user  modeling

--- Models/e-commerce/E-commerce.models.js
+++ Models/e-commerce/E-commerce.models.js
@@ -1,1 +1,25 @@
 import mongoose from "mongoose"
+
+const E_Com = new mongoose.Schema({
+    userName:{
+        type:String,
+        unique:'true',
+        required:'true',
+    },
+    password:{
+        type:String,
+        required:'true'
+    },
+    email:{
+        type:String,
+        required:'true',
+        unique:'true'
+    },
+    address:{
+        type:String,
+        required:'true'
+    }
+
+},{timestamp: 'ture'});
+
+export const e_com = mongoose.Model("ecom", E_com);
